a9e827dd76e3cf574e30974e152324ae17c5b083
fetix <78823241+feticks@users.noreply.github.com>
2021-03-21 20:30:08 +0000

    Add files via upload

---
+++ SpotifyClicker/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace night_client
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.rightclicker = new System.Windows.Forms.Timer(this.components);
+            this.leftclicker = new System.Windows.Forms.Timer(this.components);
+            this.cpslabel2 = new System.Windows.Forms.Label();
+            this.cpslabel = new System.Windows.Forms.Label();
+            this.guna2CustomCheckBox1 = new Guna.UI2.WinForms.Guna2CustomCheckBox();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.minimise = new Guna.UI2.WinForms.Guna2Button();
+            this.hide = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.slider1 = new AnyDesk.Slider();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2Elipse2 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.asdasd = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // leftclicker
+            // 
+            this.leftclicker.Tick += new System.EventHandler(this.leftclicker_Tick);
+            // 
+            // cpslabel2
+            // 
+            this.cpslabel2.AutoSize = true;
+            this.cpslabel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.cpslabel2.Font = new System.Drawing.Font("Segoe UI", 8.25F);
+            this.cpslabel2.ForeColor = System.Drawing.Color.White;
+            this.cpslabel2.Location = new System.Drawing.Point(1115, 858);
+            this.cpslabel2.Name = "cpslabel2";
+            this.cpslabel2.Size = new System.Drawing.Size(28, 13);
+            this.cpslabel2.TabIndex = 41;
+            this.cpslabel2.Text = "3:26";
+            // 
+            // cpslabel
+            // 
+            this.cpslabel.AutoSize = true;
+            this.cpslabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.cpslabel.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cpslabel.ForeColor = System.Drawing.Color.White;
+            this.cpslabel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cpslabel.Location = new System.Drawing.Point(415, 858);
+            this.cpslabel.Name = "cpslabel";
+            this.cpslabel.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cpslabel.Size = new System.Drawing.Size(28, 13);
+            this.cpslabel.TabIndex = 39;
+            this.cpslabel.Text = "0:00";
+            this.cpslabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // guna2CustomCheckBox1
+            // 
+            this.guna2CustomCheckBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2CustomCheckBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.guna2CustomCheckBox1.CheckedState.BorderColor = System.Drawing.Color.Lime;
+            this.guna2CustomCheckBox1.CheckedState.BorderRadius = 18;
+            this.guna2CustomCheckBox1.CheckedState.BorderThickness = 1;
+            this.guna2CustomCheckBox1.CheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.guna2CustomCheckBox1.CheckedState.Parent = this.guna2CustomCheckBox1;
+            this.guna2CustomCheckBox1.CheckMarkColor = System.Drawing.Color.Transparent;
+            this.guna2CustomCheckBox1.Location = new System.Drawing.Point(761, 817);
+            this.guna2CustomCheckBox1.Name = "guna2CustomCheckBox1";
+            this.guna2CustomCheckBox1.ShadowDecoration.Parent = this.guna2CustomCheckBox1;
+            this.guna2CustomCheckBox1.Size = new System.Drawing.Size(34, 34);
+            this.guna2CustomCheckBox1.TabIndex = 35;
+            this.guna2CustomCheckBox1.UncheckedState.BorderColor = System.Drawing.Color.Transparent;
+            this.guna2CustomCheckBox1.UncheckedState.BorderRadius = 0;
+            this.guna2CustomCheckBox1.UncheckedState.BorderThickness = 0;
+            this.guna2CustomCheckBox1.UncheckedState.FillColor = System.Drawing.Color.Transparent;
+            this.guna2CustomCheckBox1.UncheckedState.Parent = this.guna2CustomCheckBox1;
+            this.guna2CustomCheckBox1.CheckedChanged += new System.EventHandler(this.guna2CustomCheckBox1_CheckedChanged);
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.ResizeForm = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.pictureBox1.Location = new System.Drawing.Point(386, 858);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(61, 17);
+            this.pictureBox1.TabIndex = 55;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.pictureBox3.Location = new System.Drawing.Point(1111, 858);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(61, 17);
+            this.pictureBox3.TabIndex = 56;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(1144, -14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 17);
+            this.label1.TabIndex = 60;
+            this.label1.Text = "Rec";
+            // 
+            // minimise
+            // 
+            this.minimise.BackColor = System.Drawing.Color.Transparent;
+            this.minimise.CheckedState.Parent = this.minimise;
+            this.minimise.CustomImages.Parent = this.minimise;
+            this.minimise.FillColor = System.Drawing.Color.Transparent;
+            this.minimise.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.minimise.ForeColor = System.Drawing.Color.White;
+            this.minimise.HoverState.Parent = this.minimise;
+            this.minimise.Location = new System.Drawing.Point(1422, 1);
+            this.minimise.Name = "minimise";
+            this.minimise.PressedColor = System.Drawing.Color.White;
+            this.minimise.ShadowDecoration.Color = System.Drawing.Color.White;
+            this.minimise.ShadowDecoration.Parent = this.minimise;
+            this.minimise.Size = new System.Drawing.Size(44, 30);
+            this.minimise.TabIndex = 61;
+            this.minimise.Click += new System.EventHandler(this.minimise_Click);
+            // 
+            // hide
+            // 
+            this.hide.BackColor = System.Drawing.Color.Transparent;
+            this.hide.CheckedState.Parent = this.hide;
+            this.hide.CustomImages.Parent = this.hide;
+            this.hide.FillColor = System.Drawing.Color.Transparent;
+            this.hide.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.hide.ForeColor = System.Drawing.Color.White;
+            this.hide.HoverState.Parent = this.hide;
+            this.hide.Location = new System.Drawing.Point(1510, 2);
+            this.hide.Name = "hide";
+            this.hide.PressedColor = System.Drawing.Color.Red;
+            this.hide.ShadowDecoration.Color = System.Drawing.Color.White;
+            this.hide.ShadowDecoration.Parent = this.hide;
+            this.hide.Size = new System.Drawing.Size(50, 30);
+            this.hide.TabIndex = 63;
+            this.hide.Click += new System.EventHandler(this.hide_Click);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
+            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
+            this.guna2Button2.FillColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.HoverState.Parent = this.guna2Button2;
+            this.guna2Button2.Location = new System.Drawing.Point(1470, 2);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.PressedColor = System.Drawing.Color.White;
+            this.guna2Button2.ShadowDecoration.Color = System.Drawing.Color.White;
+            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
+            this.guna2Button2.Size = new System.Drawing.Size(38, 30);
+            this.guna2Button2.TabIndex = 64;
+            // 
+            // slider1
+            // 
+            this.slider1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.slider1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.slider1.ForeColor = System.Drawing.Color.Transparent;
+            this.slider1.Location = new System.Drawing.Point(451, 863);
+            this.slider1.Name = "slider1";
+            this.slider1.Size = new System.Drawing.Size(655, 6);
+            this.slider1.TabIndex = 29;
+            this.slider1.Value = 0F;
+            this.slider1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.slider1_MouseMove);
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 3;
+            this.guna2Elipse1.TargetControl = this.slider1;
+            // 
+            // guna2Elipse2
+            // 
+            this.guna2Elipse2.BorderRadius = 4;
+            // 
+            // asdasd
+            // 
+            this.asdasd.AutoSize = true;
+            this.asdasd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.asdasd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.asdasd.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.asdasd.ForeColor = System.Drawing.Color.LightGray;
+            this.asdasd.Location = new System.Drawing.Point(13, 846);
+            this.asdasd.Name = "asdasd";
+            this.asdasd.Size = new System.Drawing.Size(204, 17);
+            this.asdasd.TabIndex = 65;
+            this.asdasd.Text = "Spotify FREE | cheat.gq | Nightbot";
+            this.asdasd.Click += new System.EventHandler(this.asdasd_Click);
+            // 
+            // Main
+            // 
+            this.AccessibleName = "";
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1558, 885);
+            this.Controls.Add(this.asdasd);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.hide);
+            this.Controls.Add(this.minimise);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.slider1);
+            this.Controls.Add(this.guna2CustomCheckBox1);
+            this.Controls.Add(this.cpslabel2);
+            this.Controls.Add(this.cpslabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " Spotify (32 bit)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse2;
+        private System.Windows.Forms.Timer rightclicker;
+        private System.Windows.Forms.Timer leftclicker;
+        private AnyDesk.Slider slider1;
+        private Guna.UI2.WinForms.Guna2CustomCheckBox guna2CustomCheckBox1;
+        private System.Windows.Forms.Label cpslabel2;
+        private System.Windows.Forms.Label cpslabel;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Button minimise;
+        private Guna.UI2.WinForms.Guna2Button hide;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private System.Windows.Forms.Label asdasd;
+    }
+}
+
---
+++ SpotifyClicker/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Easy;
+using Memory;
+
+namespace night_client
+{
+
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+        //Left Clicker START
+
+        private void leftclicker_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                leftclicker.Interval = 1000 / Convert.ToInt32((slider1.Value * 20).ToString("0"));
+                if (guna2CustomCheckBox1.Checked == true)
+                {
+                    if (MouseButtons == MouseButtons.Left)
+                    {
+                        clickerclass.leftclick(1);
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void guna2CustomCheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (guna2CustomCheckBox1.Checked)
+            {
+                leftclicker.Start();
+            }
+            else
+            {
+                leftclicker.Stop();
+            }
+        }
+
+        private void slider1_MouseMove(object sender, MouseEventArgs e)
+        {
+            cpslabel.Text = (slider1.Value * 20).ToString("0.0") + "";
+        }
+
+        //Left Clicker STOP
+
+
+        private void minimise_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void hide_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void asdasd_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://youtube.com/Nightbot");
+            System.Diagnostics.Process.Start("http://cheat.gq");
+        }
+    }
+}
